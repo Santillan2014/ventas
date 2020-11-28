9321641624c13916de6873b00f9a1c22ceed09df
santillan2014 <diana98dafne16@gmail.com>
2020-11-28 00:34:12 -0600

    punto de ventas

---
+++ js/sweetalert.js
@@ -0,0 +1,12 @@
+Swal.fire({
+
+	title:
+	"Bienvenidas mujeres emprendedoras",
+	confirmButtonText:"Deseas saber acerca de autonomía económicade las mujeres?",
+	backdrop:true,
+	input:"text",
+	inputPlaceholder:"Nombre",
+
+	
+});
+
